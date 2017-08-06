6b7fb40faf960a574a2529b6c4fdfc5eb86ddce4
Joaquin <joaquinituarte@hotmail.com>
2017-08-06 15:58:01 -0300

    miniupdate

--- Code/Book formater/scriptFormater.py
+++ Code/Book formater/scriptFormater.py
@@ -1,6 +1,30 @@
 #Importo las funciones
 import formater
+import os
+from os import listdir
+from os.path import isfile, join
 
+
+
+#Genero la lista de paths posibles
+def get_files(folder_name):
+	directorio = os.getcwd() + folder_name
+	paths = [f for f in listdir(directorio) if isfile(join(directorio, f))]
+	lista_a_devolver = []
+	for book_name in paths:
+		lista_a_devolver.append(book_name)
+	
+	return lista_a_devolver
+
+def pick_books(books):
+
+	lista_a_devolver = []
+	for book in books:
+		respuesta = raw_input('Desea usar el siguiente libro? (y/n): '+ book +':  ')
+		if(respuesta=='y'):
+			lista_a_devolver.append(book)
+
+	return lista_a_devolver
 
 def analize_one_book(book):
 
@@ -12,8 +36,9 @@ def analize_one_book(book):
 
 
 
-books = [ 'libro1.txt', 'libro2.txt']
+files = get_files('')
+books = pick_books(files)
 
 for book in books:
-	analize_one_book(libro1)
+	analize_one_book(book)
 
