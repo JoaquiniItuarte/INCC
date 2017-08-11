f3891dccfaa081a09319db7382f981797695549b
Joaquin <joaquinituarte@hotmail.com>
2017-08-11 17:55:03 -0300

    palabras de los libros

--- print_words.py
+++ print_words.py
@@ -20,6 +20,18 @@ def print_words(book_name):
 	for word in sorted(dictionarie.keys()):
 		print word
 
+def print_words_2(book_name):
+	dictionary_folder = '/Lsaed books/Dictionaries/'
+	current_dir = os.getcwd()
+	book_path = current_dir + dictionary_folder + book_name
+
+	dictionarie = loadSaveLsaData.load_lsa_dictionary( book_path )
+
+	with open( current_dir + '/Lsaed books/ALLWORDS/' + book_name, "w+" ) as file_book:
+		for word in sorted(dictionarie.keys()):
+			file_book.write(word)
+			file_book.write('\n')
+
 def print_all_sorted_words(book):
 	similarities_folder = '/Lsaed books/Dictionaries/'
 	directorio = os.getcwd() + similarities_folder + book
@@ -34,10 +46,22 @@ def print_all_sorted_words(book):
 
 print 'Elegir UN solo libro'
 dictionary_folder = '/Lsaed books/Dictionaries/'
-book = tool.pick_books( tool.get_books(dictionary_folder) ) [0]
+books = tool.pick_books( tool.get_books(dictionary_folder) )
+
+print '****************************************************'
+print '****************************************************'
+print '****************************************************'
+print '****************************************************'
+print '****************************************************'
+print '****************************************************'
 
 respuesta = raw_input('Desea ver las palabras que contiene ( escriba r) o las similitudes (escriba s)?')
-if (respuesta == 'r'):
-	print_words(book)
-else: 
-	print_all_sorted_words(book)
+if (respuesta == 's'):
+	for book in books:
+		print_all_sorted_words(book)
+else:
+	for book in books:
+		print_words_2(book)
+
+	
+	
